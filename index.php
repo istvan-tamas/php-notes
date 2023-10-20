128aef908a0268cd9ec91182949aad9f11452c47
istvan-tamas <istvan.tamas90@gmail.com>
2023-10-20 09:04:43 +0200

    Added 404 error on invalid request

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 
     require "functions.php";
 
-    $uri = $_SERVER['REQUEST_URI'];
+    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-    if($uri === '/'){
-        require 'controllers/index.php';
-    }else if($uri === '/about'){
-        require 'controllers/about.php';
-    }else if($uri === '/contact'){
-        require 'controllers/contact.php';
+
+    $routes = [
+        '/' => 'controllers/index.php',
+        '/about' => 'controllers/about.php',
+        '/contact' => 'controllers/contact.php',
+    ];
+
+
+    if (array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    }else{
+        http_response_code(404);
+        require 'views/404.php';
+        die();
     }
